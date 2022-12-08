0c2f3920560963fe4364bb992124f847834f3c00
JoynalAbedin <jolobangladesh@gmail.com>
2022-12-08 23:03:55 +0600

    deploying backend

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,6 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// app.use(notFound);
-// app.use(errorHandler);
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -32,17 +30,19 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
-} else {
-    app.get('/', (req, res) => {
-        res.send('Server is Running...');
-    });
-}
-
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')));
+//     app.get('*', (req, res) =>
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//     );
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('Server is Running...');
+//     });
+// }
+app.get('/', (req, res) => {
+    res.send('server is connected for shopini');
+});
 app.listen(PORT, () => {
     console.log(`Server is connected on ${process.env.NODE_ENV} mode`);
 });
