21eec8f14c8487222b347a1b5baa3ca42685427d
JoynalAbedin <jolobangladesh@gmail.com>
2022-12-08 18:12:30 +0600

    vercel is deploying

--- backend/server.js
+++ backend/server.js
@@ -33,10 +33,9 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
+    app.use(express.static(path.join(__dirname, 'frontend','build')));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    
 } else {
     app.get('/', (req, res) => {
         res.send('Server is Running...');
